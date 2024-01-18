9aa0513894113c4b5cc3f7d1b1c44540270007cc
Nadira3 <paitanun35@gmail.com>
2024-01-18 06:24:31 +0100

    nullable false inclusion

--- models/user.py
+++ models/user.py
@@ -9,10 +9,10 @@ class User(BaseModel, Base):
     __tablename__ = 'users'
 
     if os.getenv('HBNB_TYPE_STORAGE') == 'db':
-        email = Column(String(128))
-        password = Column(String(128))
-        first_name = Column(String(128))
-        last_name = Column(String(128))
+        email = Column(String(128), nullable=False)
+        password = Column(String(128), nullable=False)
+        first_name = Column(String(128), nullable=False)
+        last_name = Column(String(128), nullable=False)
     else:
         email = ""
         password = ""
