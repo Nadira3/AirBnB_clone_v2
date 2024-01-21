a39081886209164ee970374f0c92816694fcefb5
Nadira3 <paitanun35@gmail.com>
2024-01-21 15:19:25 +0100

    nullable review set to false

--- models/review.py
+++ models/review.py
@@ -11,7 +11,7 @@ class Review(BaseModel, Base):
     if os.getenv("HBNB_TYPE_STORAGE") == "db":
         place_id = Column(String(60), ForeignKey("places.id"), nullable=False)
         user_id = Column(String(60), ForeignKey("users.id"), nullable=False)
-        text = Column(String(1024))
+        text = Column(String(1024), nullable=False)
     else:
         place_id = ""
         user_id = ""
