5433083b60c30c090b3d427faf8106cdfd6ece0a
Nadira3 <paitanun35@gmail.com>
2024-01-21 15:07:37 +0100

    place object mapping

--- models/place.py
+++ models/place.py
@@ -4,20 +4,20 @@ from models.base_model import BaseModel, Base
 from sqlalchemy import Column, Integer, String, ForeignKey, Float
 import os
 
-class Place(BaseModel):
+class Place(BaseModel, Base):
     """ A place to stay """
     __tablename__ =  "places"
     if os.getenv("HBNB_TYPE_STORAGE") == "db":
         city_id = Column(String(60), ForeignKey("cities.id"), nullable=False)
         user_id = Column(String(60), ForeignKey("users.id"), nullable=False)
         name = Column(String(128), nullable=False)
-        description = Column(String(1024))
+        description = Column(String(1024), nullable=True)
         number_rooms = Column(Integer, default=0, nullable=False)
         number_bathrooms = Column(Integer, default=0, nullable=False)
         max_guest = Column(Integer, default=0, nullable=False)
         price_by_night = Column(Integer, default=0, nullable=False)
-        latitude = Column(Float)
-        longitude = Column(Float)
+        latitude = Column(Float, nullable=True)
+        longitude = Column(Float, nullable=True)
     else:
         city_id = ""
         user_id = ""
